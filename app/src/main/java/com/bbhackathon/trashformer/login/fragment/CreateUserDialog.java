0a1f3094caf64820182afdaf3688989003f7d458
r88881253 <r88881253@gmail.com>
2018-12-03 22:31:35 +0800

    [feature] 增加cloud function addUser

--- app/src/main/java/com/bbhackathon/trashformer/login/fragment/CreateUserDialog.java
+++ app/src/main/java/com/bbhackathon/trashformer/login/fragment/CreateUserDialog.java
@@ -15,17 +15,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 
-import com.bbhackathon.trashformer.HomeActivity;
-import com.bbhackathon.trashformer.manager.FirebaseAuthManager;
-import com.bbhackathon.trashformer.manager.FirebaseDatabaseManager;
 import com.bbhackathon.trashformer.R;
+import com.bbhackathon.trashformer.chooseRecycleItem.ChooseRecycleItemActivity;
 import com.bbhackathon.trashformer.databinding.DialogCreateUserBinding;
+import com.bbhackathon.trashformer.manager.FirebaseAuthManager;
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.functions.FirebaseFunctions;
+import com.google.firebase.functions.FirebaseFunctionsException;
+import com.google.firebase.functions.HttpsCallableResult;
 
 import org.apache.commons.codec.digest.DigestUtils;
 
@@ -105,13 +108,74 @@ public class CreateUserDialog extends DialogFragment{
         });
     }
 
+    private void addUserToDatabase(String nickname){
+        addUserToDatabaseTask(nickname)
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Exception e = task.getException();
+                            if (e instanceof FirebaseFunctionsException) {
+                                FirebaseFunctionsException ffe = (FirebaseFunctionsException) e;
+                                FirebaseFunctionsException.Code code = ffe.getCode();
+                                Object details = ffe.getDetails();
+                            }
+                            Log.d("addUserToDatabaseTask: ","failed");
+                            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                            builder.setTitle(getString(R.string.app_name))
+                                    .setMessage(getString(R.string.add_user_to_database_failed))
+                                    .setPositiveButton(R.string.confirm, new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            getDialog().dismiss();
+                                        }
+                                    });
+                        }  else{
+                            Log.d("addUserToDatabaseTask: ","successed");
+                            startChooseRecycleItem();
+                        }
+                    }
+                });
+
+    }
+
+    private Task<String> addUserToDatabaseTask(String text) {
+        FirebaseFunctions mFunctions;
+        mFunctions = FirebaseFunctions.getInstance();
+
+        // Create the arguments to the callable function.
+        Map<String, Object> data = new HashMap<>();
+        data.put("text", text);
+        data.put("push", true);
+
+        return mFunctions
+                .getHttpsCallable("addUserToDatabase")
+                .call(data)
+                .continueWith(new Continuation<HttpsCallableResult, String>() {
+                    @Override
+                    public String then(@NonNull Task<HttpsCallableResult> task) throws Exception {
+                        // This continuation runs on either success or failure, but if the task
+                        // has failed then getResult() will throw an Exception which will be
+                        // propagated down.
+                        String result = (String) task.getResult().getData();
+                        return result;
+                    }
+                });
+    }
+
+    private void startChooseRecycleItem(){
+        Intent i = new Intent(getActivity(), ChooseRecycleItemActivity.class);
+        startActivity(i);
+    }
+
     class LoginOnCompleteListener implements OnCompleteListener<AuthResult>{
         @Override
         public void onComplete(@NonNull Task<AuthResult> task) {
             if(task.isSuccessful()){
                 Log.d(TAG, "login_background_layer_list:success\n"+task.getResult());
-                AddUserListener addUserListener = new AddUserListener(FirebaseAuthManager.getInstance().getUid(), viewmodel.getPassword(), viewmodel.getNickName());
-                FirebaseDatabaseManager.getInstance().addUser(addUserListener);
+//                AddUserListener addUserListener = new AddUserListener(FirebaseAuthManager.getInstance().getUid(), viewmodel.getPassword(), viewmodel.getNickName());
+//                FirebaseDatabaseManager.getInstance().addUser(addUserListener);
+                addUserToDatabase(viewmodel.getNickName());
             }else{
                 Log.d(TAG, "login_background_layer_list:failure\n"+ task.getException());
                 AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
@@ -147,8 +211,6 @@ public class CreateUserDialog extends DialogFragment{
             data.put("nickName", nickName);
             map.put(uid, data);
             dataSnapshot.getRef().updateChildren(map);
-            Intent i = new Intent(getActivity(), HomeActivity.class);
-            startActivity(i);
         }
 
         @Override
